d6ba5be2f234c85f928fb057e3e472839aea3a24
BenQuickDeNN <2413269761@qq.com>
2019-09-17 22:10:59 +0800

    更新parser

--- include/parser/parser.cxx
+++ include/parser/parser.cxx
@@ -1,9 +1,16 @@
+#ifndef AST_H
 #include "ast.h"	// ExprAST
-#include "lexer.h"	// gettok()
-
 #define AST_H
+#endif // !AST_H
+#ifndef LEXER_H
+#include "lexer.h"	// gettok()
 #define LEXER_H
+#endif // !LEXER_H
 
+#ifndef MAP
+#include <map>
+#define MAP
+#endif // !MAP
 #ifndef MEMORY
 #include <memory>	// unique_ptr
 #define MEMORY
@@ -100,4 +107,120 @@ static std::unique_ptr<ExprAST> ParseIdentifierExpr()
 	getNextToken();
 
 	return std::make_unique<CallExprAST>(IdName, std::move(Args));
+}
+
+/// primary
+///		::= identifierexpr
+///		::= numberexpr
+///		::= parenexpr
+static std::unique_ptr<ExprAST> ParsePrimary()
+{
+	switch (CurTok)
+	{
+	default:
+		return LogError("unknown token when expecting and expression");
+	case tok_identifier:
+		return ParseIdentifierExpr();
+	case tok_number:
+		return ParseNumberExpr();
+	case '(':
+		return ParseParenExpr();
+	}
+}
+
+/// 定义运算符的优先级
+/// BinopPrecedence - This holds the precedence for each binary operator that is
+/// defined.
+static std::map<char, int> BinopPrecedence;
+
+/// GetTokPrecedence - Get the precedence of the pending binary operator token.
+static int GetTokPrecedence()
+{
+	if (!isascii(CurTok))
+		return -1;
+
+	// Make sure it's a declared binop.
+	int TokPrec = BinopPrecedence[CurTok]; // 获取运算符优先级
+	if (TokPrec < 0) return -1;
+	return TokPrec;
+}
+
+static std::unique_ptr<ExprAST> ParseBinOpRHS(int ExprPrec,
+	std::unique_ptr<ExprAST> LHS);
+/// expression
+///		::= primary binoprhs
+///
+static std::unique_ptr<ExprAST> ParseExpression()
+{
+	auto LHS = ParsePrimary();
+	if (!LHS)
+		return nullptr;
+
+	return ParseBinOpRHS(0, std::move(LHS));
+}
+
+/// binoprhs
+///		::= ('+' primary)*
+static std::unique_ptr<ExprAST> ParseBinOpRHS(int ExprPrec,
+	std::unique_ptr<ExprAST> LHS)
+{
+	// If this is a binop, find its precedence.
+	while (1)
+	{
+		int TokePrec = GetTokPrecedence();
+
+		// If this is a binop that binds at least as tightly as the current binop
+		// consume it, otherwise we are done
+		if (TokPrec < ExprPrec)
+			return LHS;
+
+		// well this is a binop
+		int BinOp = CurTok;
+		getNextToken();	// eat binop
+
+		// Parse the primary expression after the binary operator
+		auto RHS = ParsePrimary();
+		if (!RHS)
+			return nullptr;
+
+		// 如果下一个token的优先级比当前token的优先级更高，那么就把RHS当做LHS
+		// 优先用右边两个token构造语法树节点
+		int NextPrec = GetTokPrecedence();
+		if (TokePrec < NextPrec)
+		{
+			RHS = ParseBinOpRHS(TokPrec + 1, std::move(RHS));
+			if (!RHS)
+				return nullptr;
+		}
+
+		// Merge LHS/RHS, generate AST node
+		LHS = std::make_unique<BinaryExprAST>(BinOp, std::move(LHS),
+			std::move(RHS));
+	}
+}
+
+/// prototype
+///		::= id '(' id* ')'
+static std::unique_ptr<PrototypeAST> ParsePrototype()
+{
+	if (CurTok != tok_identifier)
+		return LogErrorP("Expected function name in prototype");
+
+	std::string FnName = IdentifierStr;
+	getNextToken();
+
+	if (CurTok != '(')
+		return LogErrorP("Expected '(' in prototype");
+
+	// Read the list of argument names.
+	std::vector<std::string> ArgNames;
+	while (getNextToken() == tok_identifier)
+		ArgNames.push_back(IdentifierStr);
+	if (CurTok != ')')
+		return LogErrorP('Expected ')' in prototype');
+
+	// success
+	getNextToken();	// eat ')'
+
+	return std::make_unique<PrototypeAST>(FnName, std::move(ArgNames));
 }
--- src/kaleidoscope_main.cpp
+++ src/kaleidoscope_main.cpp
@@ -2,17 +2,37 @@
 #include "lexer.h"	// gettok()
 #define LEXER_H
 #endif // !LEXER_H
+#ifndef PARSER_H
+#include "parser.h"
+#define PARSER_H
+#endif // !PARSER_H
+
 #ifndef IOSTREAM
 #include <iostream>	// std::count, std::endl
 #define IOSTREAM
 #endif // !IOSTREAM
 
+void initialize_binopPrecedence();
 void test_lexer();
 
 int main()
 {
+	initialize_binopPrecedence();
 	test_lexer();
 	return 0;
+}
+
+/**
+* @brief 初始化二元运算符的优先级
+*/
+void initialize_binopPrecedence()
+{
+	// Install standard library operator
+	// 1 is the lowest precedence
+	BinopPrecedence['<'] = 10;
+	BinopPrecedence['+'] = 20;
+	BinopPrecedence['-'] = 20;
+	BinopPrecedence['*'] = 40;
 }
 
 void test_lexer()
