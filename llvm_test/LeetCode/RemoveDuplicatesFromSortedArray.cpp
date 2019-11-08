ff4dccdf646bd264a5da55718d87961755b010c8
BenQuickDeNN <2413269761@qq.com>
2019-11-08 20:43:14 +0800

    update RemoveDuplicatesFromSortedArray

--- llvm_test/LeetCode/RemoveDuplicatesFromSortedArray.cpp
+++ llvm_test/LeetCode/RemoveDuplicatesFromSortedArray.cpp
@@ -9,7 +9,7 @@ public:
             return nums.size();
         int count = 1;
         for (int i = 1; i < nums.size(); i++)
-		    if (nums[i] != nums[i - 1])
+		    if (nums[i] - nums[i - 1])
                 nums[count++] = nums[i];
         return count;
     }
