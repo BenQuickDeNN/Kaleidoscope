d3ca6c66868016b2b2da9703711645b724f2f893
BenQuickDeNN <2413269761@qq.com>
2019-11-27 19:18:26 +0800

    finish Permutations

--- llvm_test/LeetCode/Permutations.cpp
+++ llvm_test/LeetCode/Permutations.cpp
@@ -3,23 +3,62 @@
 using namespace std;
 
 class Solution {
-    vector<int> swap(vector<int>& nums, const int& idx1, const int& idx2)
+    template<class T> void copy_vector(const vector<T>& source, vector<T>& target)
     {
-        nums[idx1] += nums[idx2];
-        nums[idx2] = nums[idx1] - nums[idx2];
-        nums[idx1] -= nums[idx2];
-        return nums;
+        for (int i = 0; i < source.size(); i++)
+            target.push_back(source[i]);
+    }
+    template<class T> bool checkExist(const vector<T>& vec, const T& element)
+    {
+        for (int i = 0; i < vec.size(); i++)
+            if (vec[i] == element)
+                return true;
+        return false;
+    }
+    void findPermutation(vector<int>& PStack, vector<vector<int>>& res, const vector<int>& nums)
+    {
+        if (PStack.size() == nums.size())
+        {
+            vector<int> TStack;
+            copy_vector(PStack, TStack);
+            res.push_back(TStack);
+        }
+        else
+            for (int i = 0; i < nums.size(); i++)
+                if (!checkExist(PStack, nums[i]))
+                {
+                    PStack.push_back(nums[i]);
+                    findPermutation(PStack, res, nums);
+                }
+        if (!PStack.empty())
+            PStack.pop_back();
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
-        int tmpLen = 1;
-        const int lenNums = nums.size();
-        for (int i = 2; i <= lenNums; i++)
-            tmpLen *= i;
-        res.push_back(nums);
-        for (int i = 1; i < tmpLen; i++)
-            res.push_back(swap(nums, (i - 1) % lenNums, i % lenNums));
+        if (nums.empty())
+            return res;
+        /* 使用搜索树 */
+        vector<int> PStack;
+        findPermutation(PStack, res, nums);
         return res;
+    }
+};
+
+#include <algorithm>
+
+class BestSolution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> vvRet;
+        sort(nums.begin(), nums.end());
+
+        vvRet.push_back(nums);
+        while(next_permutation(nums.begin(), nums.end()))
+        {
+            vvRet.push_back(nums);
+        }
+
+        return vvRet;
     }
 };
